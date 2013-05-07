d7ec020e2d4a83fa030dcbfc7b411f89438b06db
Serge Koenigsmann <igor@doomicile.de>
2013-05-07 15:15:26 +0200

    src formatting

--- adn.php
+++ adn.php
@@ -1,51 +1,51 @@
 <?php
 
-	require 'tmhOAuth/tmhOAuth.php';
-	require 'tmhOAuth/tmhUtilities.php';
-	include("feed_generator/FeedWriter.php"); // include the feed generator feedwriter file
-	include("../wp-includes/formatting.php"); // 
-	require_once( './config.php' );
+require 'tmhOAuth/tmhOAuth.php';
+require 'tmhOAuth/tmhUtilities.php';
+include("feed_generator/FeedWriter.php"); // include the feed generator feedwriter file
+include("../wp-includes/formatting.php"); //
+require_once( './config.php' );
 
-	$lang = "de";
-	$count = 10;
-	$userid = $adn_userid; 
-	$apiurl = "https://alpha-api.app.net/stream/0/users/".$userid."/posts?count=".$count."";
-	$embedcode = true;
-    $timeout = 15;
-	
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, $apiurl);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt( $ch, CURLOPT_CONNECTTIMEOUT, $timeout );
-   	curl_setopt( $ch, CURLOPT_TIMEOUT, $timeout );
-	$output = curl_exec($ch);
-	curl_close($ch);
-	$rawData = trim($output);
+$lang = "de";
+$count = 10;
+$userid = $adn_userid;
+$apiurl = "https://alpha-api.app.net/stream/0/users/".$userid."/posts?count=".$count."";
+$embedcode = true;
+$timeout = 15;
 
-// OK?		
-	$response = json_decode($rawData, true);
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $apiurl);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt( $ch, CURLOPT_CONNECTTIMEOUT, $timeout );
+curl_setopt( $ch, CURLOPT_TIMEOUT, $timeout );
+$output = curl_exec($ch);
+curl_close($ch);
+$rawData = trim($output);
+
+// OK?
+$response = json_decode($rawData, true);
 //	print_r($response);
 
-  if ($response['meta']['code'] == 200) {
+if ($response['meta']['code'] == 200) {
 	$data = $response['data'];
 
-$feed = new FeedWriter(RSS2);
+	$feed = new FeedWriter(RSS2);
 
-$feed->setTitle($user_name.'s app.net Timeline'); // set your title
-$feed->setLink('http://'.$_SERVER['SERVER_NAME'].$_SERVER["SCRIPT_NAME"]); // set the url to the feed page you're generating
+	$feed->setTitle($user_name.'s app.net Timeline'); // set your title
+	$feed->setLink('http://'.$_SERVER['SERVER_NAME'].$_SERVER["SCRIPT_NAME"]); // set the url to the feed page you're generating
 
-$feed->setChannelElement('updated', date(DATE_ATOM , time()));
-$feed->setChannelElement('author', $user_name); // set the author name
+	$feed->setChannelElement('updated', date(DATE_ATOM , time()));
+	$feed->setChannelElement('author', $user_name); // set the author name
 
-// iterate through the facebook response to add items to the feed
-if ( is_array( $data ) ) {
-foreach($data as $entry){
+	// iterate through the facebook response to add items to the feed
+	if ( is_array( $data ) ) {
+		foreach($data as $entry){
 			$post_date_gmt = strtotime( $entry['created_at'] );
 			$post_date_gmt = gmdate( 'Y-m-d H:i:s', $post_date_gmt );
 			$post_date     = gmdate( 'd.m.Y H:i', strtotime( $entry['created_at'] ));
-			
+
 			$tweetlink = $entry["canonical_url"];
-			
+
 			$post_content 		= $entry['text'];
 			$post_content_html 	= $entry['html'];
 			$post_content = ( html_entity_decode( trim( $post_content ) ) );
@@ -55,18 +55,18 @@ foreach($data as $entry){
 					$post_content = str_replace( $url['text'], '<a href="'.$url['url'].'">'.$url['text'].'</a>', $post_content );
 				}
 			}
-//			$post_content = preg_replace('"\b(http://\S+)"', '<a href="$1">$1</a>', $post_content)."!";
-//		    $post_content = preg_replace("/(?<!http:\/\/)www\./","http://www.",$post_content);
-		    $post_content = preg_replace( "/\s((http|ftp)+(s)?:\/\/[^<>\s]+)/i", " <a href=\"\\0\" target=\"_blank\">\\0</a>",$post_content);
+			//			$post_content = preg_replace('"\b(http://\S+)"', '<a href="$1">$1</a>', $post_content)."!";
+			//		    $post_content = preg_replace("/(?<!http:\/\/)www\./","http://www.",$post_content);
+			$post_content = preg_replace( "/\s((http|ftp)+(s)?:\/\/[^<>\s]+)/i", " <a href=\"\\0\" target=\"_blank\">\\0</a>",$post_content);
 
-		    $post_content = preg_replace('/[@]+([A-Za-z0-9-_]+)/', '<a href="http://alpha.app.net/\\1" target="_blank">\\0</a>', $post_content );  
-		    $post_content = preg_replace('/[#]+([A-Za-z0-9-_]+)/', '<a href="http://alpha.app.net/hashtags/\\1" target="_blank">\\0</a>', $post_content );  
+			$post_content = preg_replace('/[@]+([A-Za-z0-9-_]+)/', '<a href="http://alpha.app.net/\\1" target="_blank">\\0</a>', $post_content );
+			$post_content = preg_replace('/[#]+([A-Za-z0-9-_]+)/', '<a href="http://alpha.app.net/hashtags/\\1" target="_blank">\\0</a>', $post_content );
 			$avatar_image_url = $entry['user']['avatar_image']['url'];
 
 			//embedcode konstruieren
 			if ($embedcode) {
 
-			$post_content = 
+				$post_content =
 			'<div class="tmb_adn_frame">
 				<div class="tmb_adn_post">
 					<a href="http://alpha.app.net/'.$entry['user']['username'].'" target="_blank" class="tmb_adn_user">
@@ -86,13 +86,13 @@ foreach($data as $entry){
 				</div>
 			</div>';
 
-//				$post_content = '<blockquote class="twitter-tweet imported"><p>'.$post_content.'</p>
-//				&mdash; '.$entry['user']['name'].' (@'.$entry['user']['username'].') <a href="'.$tweetlink.'">'.$post_date.'</a></blockquote><script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>';
+				//				$post_content = '<blockquote class="twitter-tweet imported"><p>'.$post_content.'</p>
+				//				&mdash; '.$entry['user']['name'].' (@'.$entry['user']['username'].') <a href="'.$tweetlink.'">'.$post_date.'</a></blockquote><script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>';
 			}
-			
+
 			$tags = array();
 			if ( preg_match_all( '/(^|[(\[\s])#(\w+)/', $post_content, $tag ) )
-				$tags = $tag[2];
+			$tags = $tag[2];
 
 			$reply_to     = !empty( $entry['reply_to'] ) ? $entry['reply_to'] : '';
 			$thread_id = !empty( $entry['thread_id'] ) ? $entry['thread_id'] : '';
@@ -100,30 +100,30 @@ foreach($data as $entry){
 
 			$item = $feed->createNewItem();
 
-            $item->setTitle($post_date);
-            $item->setDate($entry["created_at"]);
-            $item->setLink($tweetlink);
-            $item->setDescription($post_content);
+			$item->setTitle($post_date);
+			$item->setDate($entry["created_at"]);
+			$item->setLink($tweetlink);
+			$item->setDescription($post_content);
 			$item->addElement('guid', $tweetlink, array('isPermaLink'=>'true'));
 
 			//metagedoens
-  			$item->addElement('reply_to', $reply_to);
-  			$item->addElement('thread_id', $thread_id);
-  			$item->addElement('in_reply_to_status_id', $in_reply_to_status_id);
+			$item->addElement('reply_to', $reply_to);
+			$item->addElement('thread_id', $thread_id);
+			$item->addElement('in_reply_to_status_id', $in_reply_to_status_id);
 
 			//nach bildern suchen und enclosen
-//  			$item->setEncloser('http://www.attrtest.com', '1283629', 'audio/mpeg');
+			//  			$item->setEncloser('http://www.attrtest.com', '1283629', 'audio/mpeg');
 
 			//hashtags zu kategorien
-				foreach ( $tags as $tag ) {
-		  			$item->addElement('category', $tag);
-				}
+			foreach ( $tags as $tag ) {
+				$item->addElement('category', $tag);
+			}
 
-            $feed->addItem($item);
-        }
-// that's it... don't echo anything else, just call this method
-$feed->genarateFeed();
-  } else {
-//    return NULL;
-  }
+			$feed->addItem($item);
+		}
+		// that's it... don't echo anything else, just call this method
+		$feed->genarateFeed();
+	} else {
+		//    return NULL;
+	}
 }
--- vimeofavs.php
+++ vimeofavs.php
@@ -1,30 +1,30 @@
 <?php
 
-	include("feed_generator/FeedWriter.php"); // include the feed generator feedwriter file
-	require_once( './config.php' );
+include("feed_generator/FeedWriter.php"); // include the feed generator feedwriter file
+require_once( './config.php' );
 
-	$lang = "de";
-	$count = 10;
-	$user = $vimeo_user;
-	$apiurl = "http://vimeo.com/api/v2/".$user."/likes.json?page=1";
+$lang = "de";
+$count = 10;
+$user = $vimeo_user;
+$apiurl = "http://vimeo.com/api/v2/".$user."/likes.json?page=1";
 
-	$embedcode = true;
-    $timeout = 15;
-	
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, $apiurl);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt( $ch, CURLOPT_CONNECTTIMEOUT, $timeout );
-   	curl_setopt( $ch, CURLOPT_TIMEOUT, $timeout );
-	$output = curl_exec($ch);
-	curl_close($ch);
-	$rawData = trim($output);
+$embedcode = true;
+$timeout = 15;
 
-//if OK?		
-	$response = json_decode($rawData, true);
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $apiurl);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt( $ch, CURLOPT_CONNECTTIMEOUT, $timeout );
+curl_setopt( $ch, CURLOPT_TIMEOUT, $timeout );
+$output = curl_exec($ch);
+curl_close($ch);
+$rawData = trim($output);
+
+//if OK?
+$response = json_decode($rawData, true);
 //	print_r($response);
 
-  if (count($response)) {
+if (count($response)) {
 	$data = $response;
 
 	$feed = new FeedWriter(RSS2);
@@ -35,26 +35,26 @@
 	$feed->setChannelElement('updated', date(DATE_ATOM , time()));
 	$feed->setChannelElement('author', $user_name); // set the author name
 
-// iterate through the facebook response to add items to the feed
+	// iterate through the facebook response to add items to the feed
 	if ( is_array( $data ) ) {
-	foreach($data as $entry){
+		foreach($data as $entry){
 
 			$post_date_gmt = strtotime( $entry['liked_on'] );
 			$post_date_gmt = gmdate( 'Y-m-d H:i:s', $post_date_gmt );
 			$post_date     = $post_date_gmt;
-			
+
 			$entrylink = htmlentities($entry["url"]);
-			
-//			$post_content 		= ($entry['description']);
+
+			//			$post_content 		= ($entry['description']);
 			$title 				= $entry['title'];
-			
+
 			$author = $entry['user_name'];
 			$authorurl = $entry['user_url'];
 			$thumbnail = $entry['thumbnail_large'];
 			$video_id = $entry['id'];
 
 			$post_content = ( html_entity_decode( trim( $post_content ) ) );
-		    $post_content = preg_replace( "/\s((http|ftp)+(s)?:\/\/[^<>\s]+)/i", " <a href=\"\\0\" target=\"_blank\">\\0</a>",$post_content);
+			$post_content = preg_replace( "/\s((http|ftp)+(s)?:\/\/[^<>\s]+)/i", " <a href=\"\\0\" target=\"_blank\">\\0</a>",$post_content);
 
 			//embedcode konstruieren
 			//content & bild?
@@ -63,16 +63,16 @@
 				<div class="vimeoembed vimeo">
 				<iframe src="http://player.vimeo.com/video/'.$video_id.'?badge=0" width="625" height="352" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
 				</div>';
-// 
+				//
 				$post_content .= '<div class="ytcontent yt">(<a href="'.$entrylink.'">Video</a> von <a href="'.$authorurl.'">'.$author.'</a>)</div>';
 			}
 
 			$item = $feed->createNewItem();
 
-            $item->setTitle($title);
-            $item->setDate($post_date);
-            $item->setLink($entrylink);
-            $item->setDescription($post_content);
+			$item->setTitle($title);
+			$item->setDate($post_date);
+			$item->setLink($entrylink);
+			$item->setDescription($post_content);
 			$item->addElement('guid', $entrylink, array('isPermaLink'=>'true'));
 			$item->addElement('category', $entry['board']);
 
@@ -80,15 +80,15 @@
 			$articleimage = "";
 			$articleimage = $thumbnail;
 			if (($articleimage != "")) {
-//				$post_content .= '<img src="'.$articleimage.'" alt="'.$title.'" class="ytpreview-img attachment articleimage">';
-			  	$item->setEncloser($articleimage, "", "");
+				//				$post_content .= '<img src="'.$articleimage.'" alt="'.$title.'" class="ytpreview-img attachment articleimage">';
+				$item->setEncloser($articleimage, "", "");
 
-  				}
-            $feed->addItem($item);
-        }
-// that's it... don't echo anything else, just call this method
-$feed->genarateFeed();
-  } else {
-//    return NULL;
-  }
+			}
+			$feed->addItem($item);
+		}
+		// that's it... don't echo anything else, just call this method
+		$feed->genarateFeed();
+	} else {
+		//    return NULL;
+	}
 }
